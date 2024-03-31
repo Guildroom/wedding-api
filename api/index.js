710656b22fa2881a20dc60a62fa210b28362c545
Guildroom <bramandabagaskara01@gmail.com>
2024-03-31 22:19:02 +0800

    change inde

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express()
 
-const database = require('./config/key').MongoURI;
+const database = require('../config/key').MongoURI;
 
 mongoose.connect(database, { useNewUrlParser: true })
 const db = mongoose.connection
@@ -14,7 +14,7 @@ db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 app.use(cors())
-const commentRoutes = require('./routes/comment')
+const commentRoutes = require('../routes/comment')
 app.use('/comment', commentRoutes)
 
 app.listen(5000, () => console.log('Server Started'))
